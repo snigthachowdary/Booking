e767716d01b0dc2beea5adaa0392ab5a917aeb34
snigtha <snigthachowdary@gmail.com>
2024-04-27 04:26:01 +0530

    Your commit messeage here

---
+++ myapp/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('findbus', views.findbus, name="findbus"),
+    path('bookings', views.bookings, name="bookings"),
+    path('cancellings', views.cancellings, name="cancellings"),
+    path('seebookings', views.seebookings, name="seebookings"),
+    path('signup', views.signup, name="signup"),
+    path('signin', views.signin, name="signin"),
+    path('success', views.success, name="success"),
+    path('signout', views.signout, name="signout"),
+    path('feedback', views.feedback, name="feedback"),
+    path('feedback_logic', views.feedback_logic, name="feedback_logic"),
+]
